d6fc04631e695079c54c7a6debf26f8e155698c8
Arya <arya.vohra04@gmail.com>
2015-09-12 08:19:18 +0800

    adding admin to the posting scheme
    will this work?

--- theme/tekt/index.php
+++ theme/tekt/index.php
@@ -29,7 +29,7 @@
             <?php
             $author = get_the_author();
             $editor = get_the_modified_author();
-        if ($author == $editor) {
+        if ($author == $editor, $administrator) {
           echo "NEW RELEASE";
         }else {
           echo "EDITORIAL";

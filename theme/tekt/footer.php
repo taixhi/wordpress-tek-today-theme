82de2db2fa12291d74ab6ca59d990c0cffe6bcee
Taichi <taichikato0909@gmail.com>
2015-09-24 11:42:37 +0800

    added ad script from google
    Ads should work now

--- theme/tekt/footer.php
+++ theme/tekt/footer.php
@@ -7,7 +7,7 @@
   <a><strong>tek</strong>today. All rights reserved.</a>
   <p id="meher"> Designed by Meher Vohra</p>
   <p id="theme_cr">Theme by Arya Vohra and Taichi Kato</p>
-<a href="http://www.dmca.com/Protection/Status.aspx?ID=4eac3207-9e9b-4183-aead-85ead61f5ce0" title="DMCA.com Protection Status" class="dmca-badge"> <img src ="//images.dmca.com/Badges/dmca_protected_sml_120n.png?ID=4eac3207-9e9b-4183-aead-85ead61f5ce0"  alt="DMCA.com Protection Status" /></a>  <script src="https://streamtest.github.io/badges/streamtest.js" type="text/javascript"></script> 
+<a href="http://www.dmca.com/Protection/Status.aspx?ID=4eac3207-9e9b-4183-aead-85ead61f5ce0" title="DMCA.com Protection Status" class="dmca-badge"> <img src ="//images.dmca.com/Badges/dmca_protected_sml_120n.png?ID=4eac3207-9e9b-4183-aead-85ead61f5ce0"  alt="DMCA.com Protection Status" /></a>  <script src="https://streamtest.github.io/badges/streamtest.js" type="text/javascript"></script>
 </p>
 </footer>
 </div>
@@ -17,6 +17,6 @@
 <!-- Amazon Publisher Studio -->
 <script> var amzn_ps_tracking_id = 'tektod-20'; </script>
 <script async="true" type="text/javascript" src="//ps-us.amazon-adsystem.com/scripts/US/studio.js" charset="UTF-8"></script>
-
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </body>
 </html>

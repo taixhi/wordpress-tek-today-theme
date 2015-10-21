97a84839587d7c1625263ee302fbdae489ea36fc
Arya <arya.vohra04@gmail.com>
2015-10-21 22:49:15 +0800

    fixed again?

--- theme/tekt/functions.php
+++ theme/tekt/functions.php
@@ -155,16 +155,5 @@ function howdy_message($translated_text, $text, $domain) {
     return $new_message;
 }
 add_filter('gettext', 'howdy_message', 10, 3);
-
-//hook the administrative header output
-add_action('admin_head', 'my_custom_logo');
-
-function my_custom_logo() {
-echo '
-<style type="text/css">
-#header-logo { background-image: url('.get_bloginfo('template_directory').'/img/custom-logo.png) !important; }
-</style>
-';
-}
 ?>
 

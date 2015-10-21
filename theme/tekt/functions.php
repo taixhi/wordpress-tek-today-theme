3105c517f6f09fa07e16b3dfbac01ab869875412
Arya <arya.vohra04@gmail.com>
2015-10-21 22:48:35 +0800

    fixed?

--- theme/tekt/functions.php
+++ theme/tekt/functions.php
@@ -162,7 +162,7 @@ add_action('admin_head', 'my_custom_logo');
 function my_custom_logo() {
 echo '
 <style type="text/css">
-#header-logo { background-image: url('.get_bloginfo('template_directory').'/images/custom-logo.png) !important; }
+#header-logo { background-image: url('.get_bloginfo('template_directory').'/img/custom-logo.png) !important; }
 </style>
 ';
 }
